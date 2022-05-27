7743fa79549cfd413e668bbdf93509c44162a0b8
Gavin Capriola <gavincapriola@icloud.com>
2022-05-27 18:26:27 -0400

    initial

---
+++ ransomware/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class Ransomware:
+    def __init__(self):
+        pass
+
+    def encrypt_files(self):
+        pass
+
+    def decrypt_files(self):
+        pass
+
+    def delete_files(self):
+        pass
+
+
+if __name__ == "__main__":
+    ransomware = Ransomware()
